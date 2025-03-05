a177c0d724af0d315e7f5cf7b7969efca3ef6755
eshavkun <eeee@eeee.ee>
2025-03-05 09:50:08 +0100

    move working dir to service

--- perf/uploadAndTest.sh
+++ perf/uploadAndTest.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-WORKDIR=~/perftest/
-TESTSCRIPT=~/storagebeat/StorageBeat/perf/StorageBeat.yml
-TIMESTAMP=$(date %s)
+TESTSCRIPT=StorageBeat.yml
+TIMESTAMP=$(date +%s)
 
-cd $WORKDIR
-rm -f current_payload.csv
+mkdir -p tests
+rm -f tests/current_payload.csv
 
 STAMP=$(swarm-cli stamp list --least-used --limit 1 --quiet --hide-usage)
 for i in {1..2}; do
-  dd if=/dev/urandom of=./random_data_file bs=1K count=99
-  swarm-cli upload random_data_file --stamp $STAMP | awk '/hash/ {print $3}' >> current_payload.csv
+  dd if=/dev/urandom of=tests/random_data_file bs=1K count=99
+  swarm-cli upload tests/random_data_file --stamp $STAMP | awk '/hash/ {print $3}' >> tests/current_payload.csv
 done
 
-cp current_payload.csv ${TIMESTAMP}_swarm_payload.csv
+cp tests/current_payload.csv tests/${TIMESTAMP}_swarm_payload.csv
 
-systemd-run --user --on-active=60 artillery run -o ${TIMESTAMP}_swarm_test.txt -q -e swarm -v '{"payload":"current_payload.csv"}' $TESTSCRIPT  
+systemd-run --user --on-active=60 -d artillery run -o tests/${TIMESTAMP}_swarm_test.json -q -e swarm -v '{"payload":"tests/current_payload.csv"}' $TESTSCRIPT  
