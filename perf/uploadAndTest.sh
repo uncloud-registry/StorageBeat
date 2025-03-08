52be5e8861b696a3f14b99ea0561956eb14ea516
eshavkun <eeee@eeee.ee>
2025-03-08 10:55:31 +0100

    add arweave upload into script

--- perf/uploadAndTest.sh
+++ perf/uploadAndTest.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
+if [ "$#" -gt 0 ]; then
+  TARGET=$1
+else
+  TARGET=swarm
+fi
+
 TESTSCRIPT=StorageBeat.yml
 TIMESTAMP=$(date +%s)
 
 mkdir -p tests
-rm -f tests/current_payload.csv
+rm -f tests/current_payload_${TARGET}.csv
 
-STAMP=$(swarm-cli stamp list --least-used --limit 1 --quiet --hide-usage)
+case $TARGET in
+  swarm)
+    STAMP=$(swarm-cli stamp list --least-used --limit 1 --quiet --hide-usage)
+    UPLOAD="swarm-cli upload tests/random_data_file --stamp $STAMP | awk '/hash/ {print \$3}'"
+    ;;
+  arweave)
+    . arweave_env.sh
+    UPLOAD="ardrive upload-file --local-path tests/random_data_file -F $ARDRIVE_WALLE -w $ARDRIVE_WALLET --turbo | jq -r '.created[0].dataTxId'"
+    ;;
+esac
+
+
 for i in {1..2}; do
   dd if=/dev/urandom of=tests/random_data_file bs=1K count=99
-  swarm-cli upload tests/random_data_file --stamp $STAMP | awk '/hash/ {print $3}' >> tests/current_payload.csv
+  eval "$UPLOAD" >> tests/current_payload_${TARGET}.csv
 done
 
-cp tests/current_payload.csv tests/${TIMESTAMP}_swarm_payload.csv
+cp tests/current_payload_${TARGET}.csv tests/payload_${TARGET}_${TIMESTAMP}.csv
 
-systemd-run --user --on-active=60 -d artillery run -o tests/${TIMESTAMP}_swarm_test.json -q -e swarm -v '{"payload":"tests/current_payload.csv"}' $TESTSCRIPT  
+systemd-run --user --on-active=60 -d artillery run -o tests/${TIMESTAMP}_${TARGET}_test.json -q -e $TARGET -v '{"payload":"tests/current_payload_'${TARGET}'.csv"}' $TESTSCRIPT  
