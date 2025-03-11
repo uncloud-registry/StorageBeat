376405ddefe6840e1e67ecbf2d634a341a0ec131
eshavkun <eeee@eeee.ee>
2025-03-11 11:01:56 +0100

    Add target to service setup

--- perf/setupService.sh
+++ perf/setupService.sh
@@ -2,8 +2,9 @@
 
 mkdir -p ~/.config/systemd/user/
 
-sed "s%WORKDIR%$(pwd)%g" uploadAndTest.service.template > ~/.config/systemd/user/uploadAndTest.service
-cp uploadAndTest.timer ~/.config/systemd/user/uploadAndTest.timer
+sed "s%WORKDIR%$(pwd)%g" uploadAndTest.service.template > ~/.config/systemd/user/uploadAndTest${1}.service
+sed "s%TARGET%${1}%g" -i ~/.config/systemd/user/uploadAndTest${1}.service
+cp uploadAndTest.timer ~/.config/systemd/user/uploadAndTest${1}.timer
 
 chmod +x uploadAndTest.sh
 
