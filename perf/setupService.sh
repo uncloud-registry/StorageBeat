933c084c3f5c10187a2b52a738a834ba30608722
eshavkun <eeee@eeee.ee>
2025-03-12 15:56:41 +0100

    update service setup

--- perf/setupService.sh
+++ perf/setupService.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+TARGET="${1:-dryrun}"
+FILESIZE="${3:-99}"
+[[ ${4} != M ]] && SIZEUNIT=K || SIZEUNIT=M
+ARGS=${@}
+
 mkdir -p ~/.config/systemd/user/
 
-sed "s%WORKDIR%$(pwd)%g" uploadAndTest.service.template > ~/.config/systemd/user/uploadAndTest${1}.service
-sed "s%TARGET%${1}%g" -i ~/.config/systemd/user/uploadAndTest${1}.service
-cp uploadAndTest.timer ~/.config/systemd/user/uploadAndTest${1}.timer
+sed "s%WORKDIR%$(pwd)%g" uploadAndTest.service.template > ~/.config/systemd/user/uploadAndTest_${TARGET}_${FILESIZE}${SIZEUNIT}.service
+sed "s|ARGS|${ARGS}|g" -i ~/.config/systemd/user/uploadAndTest_${TARGET}_${FILESIZE}${SIZEUNIT}.service
+sed "s|PARAMS|${TARGET}_${FILESIZE}${SIZEUNIT}|g" uploadAndTest.timer.template > ~/.config/systemd/user/uploadAndTest_${TARGET}_${FILESIZE}${SIZEUNIT}.timer
 
 chmod +x uploadAndTest.sh
 
