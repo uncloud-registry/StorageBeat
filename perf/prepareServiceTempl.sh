e64286c6a077b6ef61e220375bdf099cd395d4fa
eshavkun <eeee@eeee.ee>
2025-03-27 20:06:06 +0100

    fix path

--- perf/prepareServiceTempl.sh
+++ perf/prepareServiceTempl.sh
@@ -5,8 +5,8 @@ mkdir -p ~/.config/systemd/user/
 cp uploadAndTest@.timer ~/.config/systemd/user/
 sed "s%WORKDIR%$(pwd)%g" uploadAndTest@.service > ~/.config/systemd/user/uploadAndTest@.service
 
-if [ ! -f ~/.config/systemd/environment.d/10-path.conf ]; then
-    mkdir -p ~/.config/systemd/environment.d
+if [ ! -f ~/.config/environment.d/10-path.conf ]; then
+    mkdir -p ~/.config/environment.d
     echo "PATH=${PATH}" > ~/.config/environment.d/10-path.conf
     systemctl --user daemon-reload
 fi
