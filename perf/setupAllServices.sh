4ca60ee8c751424b585b78babbd229cff1f95623
eshavkun <eeee@eeee.ee>
2025-03-31 18:51:59 +0200

    Add multiple services setup script

--- perf/setupAllServices.sh
+++ perf/setupAllServices.sh
@@ -1,9 +1,9 @@
-# systemd-run --user --on-active=60 -d ./setupServiceTempl.sh ipfs 100 100 K
+#!/bin/bash
 
 targets=("ipfs" "arweave" "swarm" "s3")
 loads=("100 100 K" "100 1 M" "100 10 M" "1 100 M")
 loadsArweave=("10 100 K" "100 100 K" "10 1 M" "1 10 M")
-delay=0
+delay=10
 
 
 
@@ -14,8 +14,8 @@ for i in {0..3}; do
         else
             load=${loadsArweave[$i]}
         fi
-        systemd-run --user --on-active="${delay}h 10m" -d ./setupServiceTempl.sh ${target} ${load}
-        ((delay++))
+        systemd-run --user --on-active=${delay}m -d ./setupServiceTempl.sh ${target} ${load}
+        ((delay+=90))
     done
 done
 
