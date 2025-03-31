17daba13d726e668218fcea17e44da86e490d317
eshavkun <eeee@eeee.ee>
2025-03-31 18:46:05 +0200

    Rename unit files and fix path

--- perf/setupService.sh
+++ perf/setupService.sh
@@ -9,15 +9,18 @@ BMARK=${TARGET}_${FILENUM}_${FILESIZE}${SIZEUNIT}
 
 mkdir -p ~/.config/systemd/user/
 
-sed "s%WORKDIR%$(pwd)%g" uploadAndTest.service.template > ~/.config/systemd/user/uploadAndTest_${BMARK}.service
+sed "s%WORKDIR%$(pwd)%g" uploadAndTest.service > ~/.config/systemd/user/uploadAndTest_${BMARK}.service
 sed "s|ARGS|${ARGS}|g" -i ~/.config/systemd/user/uploadAndTest_${BMARK}.service
-sed "s|PARAMS|${BMARK}|g" uploadAndTest.timer.template > ~/.config/systemd/user/uploadAndTest_${BMARK}.timer
+sed "s|PARAMS|${BMARK}|g" uploadAndTest.timer > ~/.config/systemd/user/uploadAndTest_${BMARK}.timer
 
-if [ ! -f ~/.config/systemd/user/environment.d/10-path.conf ]; then
-    mkdir -p ~/.config/systemd/user/environment.d/10-path.conf
+if [ ! -f ~/.config/environment.d/10-path.conf ]; then
+    mkdir -p ~/.config/environment.d/10-path.conf
     echo "PATH=${PATH}" > ~/.config/environment.d/10-path.conf
     systemctl --user daemon-reload
 fi
 
 chmod +x uploadAndTest.sh
+
+systemctl --user enable uploadAndTest_${BMARK}.timer
+systemctl --user start uploadAndTest_${BMARK}.timer
 
